62b01d80ec59de0735c5d51bcbe37e80921654b1
Omar <omarattia3143@gmail.com>
2022-01-26 16:04:43 -0800

    jump on clouds solution added

---
+++ JumpingontheClouds/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JumpingontheClouds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>
+            {
+                0 ,0 ,0, 1, 0, 0
+            };
+            var result = jumpingOnClouds(list);
+
+            Console.WriteLine(result);
+
+        }
+
+
+        public static int jumpingOnClouds(List<int> c)
+        {
+            int steps = 0;
+
+            for (var i = 0; i < c.Count - 1;)
+            {
+                if (c.Count - i > 2)
+                {
+                    if (c[i + 2] == 0)
+                    {
+                        i += 2;
+                        steps++;
+                    }
+                    else
+                    {
+                        i++;
+                        steps++;
+                    }
+                }
+                else
+                {
+                    if (c[i + 1] == 0)
+                    {
+                        steps++;
+                        i++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return steps;
+        }
+    }
+}
