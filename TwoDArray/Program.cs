f291b0d681a528c86bd4b6ccd121fed06c0e7596
Omar <omarattia3143@gmail.com>
2022-01-30 14:33:20 -0800

    2D array - DS solution added

---
+++ TwoDArray/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TwoDArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        
+        public static int hourglassSum(List<List<int>> arr)
+        {
+            List<int> localArray = new List<int>();
+            List<int> finalArray = new List<int>();
+        
+            for(var i=0;i<4;i++)
+            {
+                for(var j=0;j<4;j++)
+                {
+                    localArray.Add(arr[i][j]);
+                    localArray.Add(arr[i][j+1]);
+                    localArray.Add(arr[i][j+2]);
+                
+                    localArray.Add(arr[i+1][j+1]);
+                
+                    localArray.Add(arr[i+2][j]);
+                    localArray.Add(arr[i+2][j+1]);
+                    localArray.Add(arr[i+2][j+2]);
+                
+                    finalArray.Add(localArray.Sum());
+                    localArray.Clear();
+                }
+            }
+            return finalArray.Max();
+        }
+
+    }
+}
